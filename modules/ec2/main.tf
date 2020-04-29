238a18ec0cfced765409e848fc805f76d9ee9657
THC-QA <Tristan.Clapp@academytrainee.com>
2020-04-29 10:45:36 +0100

    files

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "jenkins" {
-  ami                    = "ami-0c55b159cbfafe1f0"
+  ami                    = var.jenkins-ami
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
   
@@ -11,9 +11,17 @@ resource "aws_instance" "jenkins" {
   }
 }
 resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"  ingress {
+  name = "terraform-example-instance"
+  
+  ingress {
     from_port   = 8080
     to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 9600
+    to_port     = 9600
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
