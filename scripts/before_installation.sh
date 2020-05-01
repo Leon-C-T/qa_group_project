061e80c3fb551b4e51cd8a5f4a1e0fd3a3e288ba
Ubuntu <ubuntu@ip-13-12-21-171.eu-west-2.compute.internal>
2020-05-01 09:12:41 +0000

    before_in updated

--- scripts/before_installation.sh
+++ scripts/before_installation.sh
@@ -12,4 +12,4 @@ sudo systemctl status docker
 sudo apt update
 sudo apt install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt install ansible -y
