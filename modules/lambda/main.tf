ef0c67f3a835ceb3330a901b0321cb4d9ce5c2f9
Ubuntu <ubuntu@ip-22-10-10-239.eu-west-2.compute.internal>
2020-05-01 19:58:31 +0000

    lambda policies

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "lambda-snapshot" {
-  filename      = "lambda_function_payload.zip"
+  filename      = "../../payloads/snapshot.zip"
   function_name = "ec2_bidaily_snapshot"
-  role          = "${aws_iam_role.iam_for_lambda.arn}"
+  role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda_function.snapshot"
   vpc_config {
       subnet_ids         = var.lambda-subnet-ids
@@ -13,9 +13,9 @@ resource "aws_lambda_function" "lambda-snapshot" {
 }
 
 resource "aws_lambda_function" "lambda-recovery" {
-  filename      = "lambda_function_payload.zip"
+  filename      = "../../payloads/recovery.zip"
   function_name = "jenkins_recovery_function"
-  role          = "${aws_iam_role.iam_for_lambda.arn}"
+  role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda_function.recovery"
   vpc_config {
       subnet_ids         = var.lambda-subnet-ids
@@ -24,4 +24,48 @@ resource "aws_lambda_function" "lambda-recovery" {
 
   runtime = "python3.8"
   timeout = 120
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "Lambda-Role"
+
+  assume_role_policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+                "ec2:CreateNetworkInterface",
+                "ec2:DescribeNetworkInterfaces",
+                "ec2:DeleteNetworkInterface"
+            ],
+            
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "AWSLambdaBasicExecutionRole" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.iam_for_lambda.name
+}
+
+resource "aws_iam_role_policy_attachment" "AWSLambdaVPCAccessExecutionRole" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+  role       = aws_iam_role.iam_for_lambda.name
+}
+
+resource "aws_iam_role_policy_attachment" "lambda-EC2InstanceConnect" {
+  policy_arn = "arn:aws:iam::aws:policy/EC2InstanceConnect"
+  role       = aws_iam_role.iam_for_lambda.name
+}
+
+resource "aws_iam_role_policy_attachment" "lambda-AmazonEC2FullAccess" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+  role       = aws_iam_role.iam_for_lambda.name
 }
