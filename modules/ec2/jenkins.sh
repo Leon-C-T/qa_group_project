751120a13df6f087368ee210b12840ab7a0b7ff7
THC-QA <Tristan.Clapp@academytrainee.com>
2020-05-03 15:47:18 +0100

    sudoers

--- modules/ec2/jenkins.sh
+++ modules/ec2/jenkins.sh
@@ -13,4 +13,5 @@ sudo apt-get install jenkins -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo apt install unzip -y
 sudo ./aws/install 
+echo 'jenkins ALL=(ALL:ALL) NOPASSWD: ALL' >> /etc/sudoers
 sleep 30
