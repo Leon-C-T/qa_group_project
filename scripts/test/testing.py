c8fd1bf249d1e0d10cf5e77a411f0725880edd0f
Ubuntu <ubuntu@ip-13-12-21-171.eu-west-2.compute.internal>
2020-05-03 15:41:27 +0000

    changes to the url layout

--- scripts/test/testing.py
+++ scripts/test/testing.py
@@ -2,9 +2,10 @@ import pytest
 import requests
 import os
 
-url = os.environ['url']
+urt = os.environ['url']
 
 ############################################################### testing url ###############################################################
 def test_urlmanager_home():
+    url = 'http://' + urt
     r = requests.get(url)
     assert r.status_code == 200
