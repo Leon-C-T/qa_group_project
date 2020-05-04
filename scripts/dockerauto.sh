381929730dba0f1e6776f1fef82e6262bef86f36
thenu97 <thenuja_1@outlook.com>
2020-05-04 22:24:04 +0000

    cred not exposed in container

--- scripts/dockerauto.sh
+++ scripts/dockerauto.sh
@@ -5,5 +5,5 @@ docker login --username=$DOCKER_USER --password=$DOCKER_PASS
 docker build -t thenu97/frontend /var/lib/jenkins/workspace/latte/spring-petclinic-ang/.
 docker push thenu97/frontend
 sleep 20
-docker build -t thenu97/backendpre /var/lib/jenkins/workspace/latte/spring-petclinic-res/.
-docker push thenu97/backendpre
+docker build --build-arg url=${url} --build-arg password=${password} --build-arg username=${username} -t thenu97/backendup /var/lib/jenkins/workspace/latte/spring-petclinic-res/.
+docker push thenu97/backendup
