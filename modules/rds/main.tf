b94f245338e00f52c5238f79f7539f58775e619d
THC-QA <Tristan.Clapp@academytrainee.com>
2020-05-04 02:59:41 +0100

    RDS rds RDS

---
+++ modules/rds/main.tf
@@ -0,0 +1,20 @@
+resource "aws_db_instance" "petclinic" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "petclinic"
+  username             = var.username
+  password             = var.password
+  parameter_group_name = "default.mysql5.7"
+}
+
+resource "aws_db_subnet_group" "petclinic-subnet-group" {
+  name       = "main"
+  subnet_ids = ["${var.subnetA}", "${var.subnetB}"]
+
+  tags = {
+    Name = "Petclinic DB subnet group"
+  }
+}
