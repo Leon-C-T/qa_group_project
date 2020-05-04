186ea59a448acf0c5da37ead8f3a5e25c18ded6f
thenu97 <thenuja_1@outlook.com>
2020-05-04 14:34:50 +0000

    script and dockerfile changed for external db

--- scripts/dockerauto.sh
+++ scripts/dockerauto.sh
@@ -5,5 +5,5 @@ docker login --username=$DOCKER_USER --password=$DOCKER_PASS
 docker build -t thenu97/frontend /var/lib/jenkins/workspace/latte/spring-petclinic-ang/.
 docker push thenu97/frontend
 sleep 20
-docker build -t thenu97/backend /var/lib/jenkins/workspace/latte/spring-petclinic-res/.
-docker push thenu97/backend
+docker build -t thenu97/backendpre /var/lib/jenkins/workspace/latte/spring-petclinic-res/.
+docker push thenu97/backendpre
