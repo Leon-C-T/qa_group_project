0c653ba675b990bc96cfed03a121599412f8dae2
thenu97 <thenuja_1@outlook.com>
2020-05-04 10:54:39 +0000

    external database added

--- scripts/dockerauto.sh
+++ scripts/dockerauto.sh
@@ -2,5 +2,8 @@
 sudo usermod -aG docker $USER
 source ~/.bashrc
 docker login --username=$DOCKER_USER --password=$DOCKER_PASS
-docker build -t thenu97/frontend /var/lib/jenkins/workspace/latte/spring-petclinic-angular-1/.
+docker build -t thenu97/frontend /var/lib/jenkins/workspace/latte/spring-petclinic-ang/.
 docker push thenu97/frontend
+sleep 20
+docker build -t thenu97/backend /var/lib/jenkins/workspace/latte/spring-petclinic-res/.
+docker push thenu97/backend
