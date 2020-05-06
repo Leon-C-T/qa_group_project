78be257a3ea000788fa492222baf98c2970b4bcd
thenu97 <thenuja_1@outlook.com>
2020-05-06 23:10:22 +0000

    reverting back

--- scripts/dockerauto.sh
+++ scripts/dockerauto.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-sudo gpasswd -a jenkins docker
-sudo apt install mysql-client-core-8.0
+sudo usermod -aG docker $USER
 source ~/.bashrc
-mysql -h $url -u $username -p$password "petclinic" < /var/lib/jenkins/workspace/latte/spring-petclinic-res/src/main/resources/db/mysql/initDB.sql
-mysql -h $url -u $username -p$password "petclinic" < /var/lib/jenkins/workspace/latte/spring-petclinic-res/src/main/resources/db/mysql/populateDB.sql
 docker login --username=$DOCKER_USER --password=$DOCKER_PASS
 docker build -t thenu97/frontend /var/lib/jenkins/workspace/latte/spring-petclinic-ang/.
 docker push thenu97/frontend
