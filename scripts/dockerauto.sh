1d42f3b1844c45e109a91a2cfce33ba99686eb18
thenu97 <thenuja_1@outlook.com>
2020-05-06 22:48:26 +0000

    jenkins reboot for docker not needed

--- scripts/dockerauto.sh
+++ scripts/dockerauto.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-sudo usermod -aG docker $USER
+gpasswd -a jenkins docker
+sudo apt install mysql-client-core-8.0
 source ~/.bashrc
+mysql -h $url -u $username -p$password "petclinic" < /var/lib/jenkins/workspace/latte/spring-petclinic-res/src/main/resources/db/mysql/initDB.sql
+mysql -h $url -u $username -p$password "petclinic" < /var/lib/jenkins/workspace/latte/spring-petclinic-res/src/main/resources/db/mysql/populateDB.sql
 docker login --username=$DOCKER_USER --password=$DOCKER_PASS
 docker build -t thenu97/frontend /var/lib/jenkins/workspace/latte/spring-petclinic-ang/.
 docker push thenu97/frontend
