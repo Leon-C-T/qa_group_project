5f8773b9e83721f8ceb4729267be9663fbbad763
thenu97 <thenuja_1@outlook.com>
2020-05-06 22:57:51 +0000

    start docker

--- scripts/before_installation.sh
+++ scripts/before_installation.sh
@@ -2,5 +2,7 @@
 sudo apt update -y
 sudo apt-get install python3-pip -y
 sudo apt install docker-compose -y
+sudo systemctl start docker
+sudo systemctl enable docker
 sudo systemctl status docker
 sudo apt update
