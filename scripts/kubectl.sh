c82fa4d81227d0af001a42e6ac56c65b60dd8e55
Ubuntu <ubuntu@ip-13-12-21-227.eu-west-2.compute.internal>
2020-05-07 06:19:48 +0000

    resources added to backend

--- scripts/kubectl.sh
+++ scripts/kubectl.sh
@@ -1,7 +1,7 @@
 curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
-echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+sudo echo 'export PATH=$PATH:$HOME/bin' >> sudo ~/.bashrc
 kubectl version --short --client
 aws eks update-kubeconfig --name PetClinic
 sleep 20
